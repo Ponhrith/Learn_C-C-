8e3a314e35fb1eb5bee2b8ddb71f26788542949d
Ponhrith <ponhrith.nhep@gmail.com>
2022-12-26 15:38:35 +0700

    Testing

---
+++ Loop/99_Bottles.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+
+  
+  for (int i = 99; i > 0; i--){
+    std::cout <<i <<" "<<"bottles of pop on the wall.\n Take one down and pass it around.\n";
+  }
+  
+}
