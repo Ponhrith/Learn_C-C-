c5ea55b2e4f338101adcc4dcaf6782cab81bdbb9
Ponhrith <ponhrith.nhep@gmail.com>
2022-12-26 17:36:27 +0700

    Testing

--- Loop/FizzBuss.cpp
+++ Loop/FizzBuss.cpp
@@ -2,19 +2,17 @@
 int main(){
   
   for (int i = 1; i <= 100; i++){
-    std::cout<< i;"\n";
 
-    if (i % 3 == 0){
-    std::cout <<"Fizz\n";
-  } 
-  else if (i % 5 == 0){
-    std::cout <<"Buzz\n";
-  }
-  else if (i % 3 == 0 && i % 5 == 0){
-    std::cout <<"FizzBuzz\n";
-  }
+    if (i % 15 == 0) {
+      std::cout << "FizzBuzz\n";
+    } else if (i % 5 == 0) {
+      std::cout << "Buzz\n";
+    } else if (i % 3 == 0) {
+      std::cout << "Fizz\n";
+    } else {
+      std::cout << i << "\n";
+    }
 
   }
-
 
 }
