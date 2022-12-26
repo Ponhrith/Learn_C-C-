4f5b7a96cac421875b6e564c9a661b43addd3cb7
Ponhrith <ponhrith.nhep@gmail.com>
2022-12-26 15:09:06 +0700

    Testing

---
+++ Loop/While_Loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  
+  int i = 0;
+  int square = 0;
+  
+  
+  while (i < 10 & square < 10){
+    std::cout << i <<" "<< i * square << "\n";
+
+    i++;
+    square++;
+  }
+  
+}
