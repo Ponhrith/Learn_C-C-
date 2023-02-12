aa8d2cce88cf28d89604fbc98de765ac58d992b9
Ponhrith <ponhrith.nhep@gmail.com>
2023-02-12 19:48:33 +0700

    Constructor

--- OOP/CodeBeauty_OOP.cpp
+++ OOP/CodeBeauty_OOP.cpp
@@ -13,21 +13,27 @@ public:
         std::cout << "Age -" << Age << std::endl;
     }
 
+    Employee(string name, string company, int age){
+        Name = name;
+        Company = company;
+        Age = age;
+    }
+
 
 };
 
 int main()
 {
-    Employee employee1;
-    employee1.Name = "Pony";
-    employee1.Company = "Dolphin";
-    employee1.Age = 16;
+    Employee employee1 = Employee("Pony", "Dolphin", 16);
+    // employee1.Name = "Pony";
+    // employee1.Company = "Dolphin";
+    // employee1.Age = 16;
     employee1.IntroduceYourself();
 
-    Employee employee2;
-    employee1.Name = "Le Petit Prince";
-    employee1.Company = "Earth";
-    employee1.Age = 12;
+    Employee employee2 = Employee("Le Petit Prince", "Earth", 12);
+    // employee1.Name = "Le Petit Prince";
+    // employee1.Company = "Earth";
+    // employee1.Age = 12;
     employee1.IntroduceYourself();
 
 
